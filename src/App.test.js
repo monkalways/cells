6ed81fdeef8e88378ca8609f772d67cf32200bfa
Wei Wei <wei.wei@edmontonpolice.ca>
2018-10-04 10:29:30 -0600

    Update sample test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+it('sample test', () => {
+  const a = 1;
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  expect(a).toBe(1);
 });
+
