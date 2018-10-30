190d444e06e471d339d2087d84d6b2b8ec8fb47d
Wei Wei <wei.wei@edmontonpolice.ca>
2018-10-30 08:48:24 -0600

    Update Logo and route

--- src/app/TapToBeginPage/TapToBeginPage.js
+++ src/app/TapToBeginPage/TapToBeginPage.js
@@ -1,38 +1,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Grid, Typography, withStyles } from '@material-ui/core';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+
+import { LAYOUT } from '../constants';
 import logo from './EPSlogo.png';
 
 const propTypes = {
   classes: PropTypes.shape({}).isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
-const TapToBeginPage = ({ classes }) => (
-  <Grid container justify="center">
-    <Grid
-      container
-      className={classes.container}
-      alignItems="center"
-      justify="center"
-    >
-      <Grid item>
-        <img src={logo} alt="EPS Logo" width={200} />
-        <br />
-        <br />
-        <Typography variant="h4">Tap to begin</Typography>
+const TapToBeginPage = ({ classes, history, match }) => {
+  const handleClick = () => {
+    const { name } = match.params;
+    history.push(`/cells/${name}/overview`);
+  };
+  return (
+    <Grid container justify="center">
+      <Grid
+        container
+        className={classes.container}
+        alignItems="center"
+        justify="center"
+        onClick={handleClick}
+      >
+        <Grid item>
+          <img src={logo} alt="EPS Logo" width={350} />
+          <Typography variant="h4" className={classes.heading} align="center">
+            Tap to begin
+          </Typography>
+        </Grid>
       </Grid>
     </Grid>
-  </Grid>
-);
+  );
+};
 
 TapToBeginPage.propTypes = propTypes;
 
-export default withStyles((theme) => ({
-  container: {
-    height: 720,
-    background: theme.palette.background.default,
-    [theme.breakpoints.up('lg')]: {
-      width: 1280,
+export default compose(
+  withStyles((theme) => ({
+    container: {
+      height: LAYOUT.height,
+      background: '#3D89F7', // TODO: set this color in theme config
+      [theme.breakpoints.up('lg')]: {
+        width: LAYOUT.width,
+      },
+      cursor: 'pointer',
     },
-  },
-}))(TapToBeginPage);
+    heading: {
+      marginTop: 20,
+      color: '#FFFFFF', // TODO: set this color in theme config
+    },
+  })),
+  withRouter,
+)(TapToBeginPage);
