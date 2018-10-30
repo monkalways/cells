82aef69f8326ef7df773f8441794ddccf65260f4
Wei Wei <wei.wei@edmontonpolice.ca>
2018-10-30 15:22:22 -0600

    Remove unused import

--- src/app/Cell/CellComponent.js
+++ src/app/Cell/CellComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Typography, withStyles } from '@material-ui/core';
+import { withStyles } from '@material-ui/core';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
 
