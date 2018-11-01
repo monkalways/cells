6d042da0150595aaf3593953e6a7622f4b12ba68
Wei Wei <wei.wei@edmontonpolice.ca>
2018-11-01 11:04:01 -0600

    Add mui theme

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 
 import App from './app/App';
 import configureStore from './store';
+import theme from './mui-theme';
 
 const reduxStore = configureStore(window.REDUX_INITIAL_DATA);
 
 const RootHtml = () => (
   <ReduxProvider store={reduxStore}>
     <CssBaseline>
-      <Router>
-        <App />
-      </Router>
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <App />
+        </Router>
+      </MuiThemeProvider>
     </CssBaseline>
   </ReduxProvider>
 );
