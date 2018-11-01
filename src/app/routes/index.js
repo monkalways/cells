b51f24155de3d81f4ceeab872dd7056a3b3bb4e5
Zachary Petriw <zachary.petriw@edmontonpolice.ca>
2018-11-01 14:16:07 -0600

    Add detainee/:id route to application

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,5 +1,6 @@
 import TapToBeginPage from '../TapToBeginPage';
 import Cell from '../Cell';
+import Detainee from '../Detainee';
 
 const routes = [
   {
@@ -10,6 +11,11 @@ const routes = [
   {
     path: '/cells/:name/home',
     component: Cell,
+    exact: false,
+  },
+  {
+    path: '/detainees/:id',
+    component: Detainee,
     exact: false,
   },
 ];
