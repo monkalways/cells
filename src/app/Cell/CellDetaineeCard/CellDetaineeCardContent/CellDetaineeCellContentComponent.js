230a0fd5e9cc268270eae95d54bb7d67add946d6
Wei Wei <wei.wei@edmontonpolice.ca>
2018-11-01 15:28:35 -0600

    Add click handler

--- src/app/Cell/CellDetaineeCard/CellDetaineeCardContent/CellDetaineeCellContentComponent.js
+++ src/app/Cell/CellDetaineeCard/CellDetaineeCardContent/CellDetaineeCellContentComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { CardMedia, withStyles } from '@material-ui/core';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 
 import detaineeImage from './detainee.PNG';
 import detaineeAnonymousImage from './detainee-anonymous.PNG';
@@ -8,16 +10,21 @@ import detaineeAnonymousImage from './detainee-anonymous.PNG';
 const propTypes = {
   classes: PropTypes.shape({}).isRequired,
   cellDetainee: PropTypes.shape({
+    id: PropTypes.string.isRequired,
     firstName: PropTypes.string.isRequired,
     lastName: PropTypes.string.isRequired,
   }).isRequired,
   isAuthenticated: PropTypes.bool.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 const CellDetaineeCardContentComponent = ({
   classes,
   cellDetainee,
   isAuthenticated,
+  history,
 }) => {
   const getDetaineeImage = () => {
     if (isAuthenticated) {
@@ -29,20 +36,31 @@ const CellDetaineeCardContentComponent = ({
     }
     return detaineeAnonymousImage;
   };
+
+  const handleClick = () => {
+    if (isAuthenticated) {
+      history.push(`/detainees/${cellDetainee.id}`);
+    }
+  };
+
   return (
     <CardMedia
       className={classes.media}
       image={getDetaineeImage()}
       title={`${cellDetainee.firstName} ${cellDetainee.lastName}`}
+      onClick={handleClick}
     />
   );
 };
 
 CellDetaineeCardContentComponent.propTypes = propTypes;
 
-export default withStyles(() => ({
-  media: {
-    height: 0,
-    paddingTop: '66.25%', // 16:9
-  },
-}))(CellDetaineeCardContentComponent);
+export default compose(
+  withStyles(() => ({
+    media: {
+      height: 0,
+      paddingTop: '66.25%', // 16:9
+    },
+  })),
+  withRouter,
+)(CellDetaineeCardContentComponent);
