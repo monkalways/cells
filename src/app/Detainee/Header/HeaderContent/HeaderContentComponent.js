86697d189c854cb3030cfbac609fa655173c5a50
Zachary Petriw <zachary.petriw@edmontonpolice.ca>
2018-11-02 18:12:02 -0600

    Change "Cell" label color to red

--- src/app/Detainee/Header/HeaderContent/HeaderContentComponent.js
+++ src/app/Detainee/Header/HeaderContent/HeaderContentComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Typography, withStyles } from '@material-ui/core';
+import { Grid, Typography, withStyles } from '@material-ui/core';
 
 const propTypes = {
   classes: PropTypes.shape({}).isRequired,
@@ -8,17 +8,30 @@ const propTypes = {
 };
 
 const HeaderContent = ({ classes, cellName }) => (
-  <React.Fragment>
-    <Typography variant="h4" align="center" className={classes.root}>
-      {`Cell: ${cellName}`}
-    </Typography>
-  </React.Fragment>
+  <Grid container justify="center" className={classes.grid}>
+    <Grid item sm={5}>
+      <Typography variant="h4" className={classes.headerTitle}>
+        Cell:
+      </Typography>
+      <Typography variant="h4" className={classes.headerContent}>
+        {cellName}
+      </Typography>
+    </Grid>
+  </Grid>
 );
 
 HeaderContent.propTypes = propTypes;
 
-export default withStyles(() => ({
+export default withStyles((theme) => ({
   root: {
     flexGrow: 1,
+  },
+  headerTitle: {
+    display: 'inline',
+    color: 'red',
+    marginRight: theme.spacing.unit,
+  },
+  headerContent: {
+    display: 'inline',
   },
 }))(HeaderContent);
