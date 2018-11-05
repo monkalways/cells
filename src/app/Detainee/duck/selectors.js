a08dbaad258cb74565ab54b3aec2b3ffac8a47c2
Zachary Petriw <zachary.petriw@edmontonpolice.ca>
2018-11-05 14:58:27 -0700

    Remove console log statements from Detainee selectors

--- src/app/Detainee/duck/selectors.js
+++ src/app/Detainee/duck/selectors.js
@@ -1,24 +1,15 @@
 import { createSelector } from 'reselect';
 
 // Basic selectors
-const getDetainee = (state) => {
-  console.log(state);
-  return state.detainee.detainee.data;
-};
+const getDetainee = (state) => state.detainee.detainee.data;
 const isDetaineeProfileLoaded = (state) => state.detainee.detainee.loaded;
 
 // Reselect selectors
-const getDetaineeState = createSelector([getDetainee], (detainee) => {
-  console.log(`selector getDetaineeState: detainee = ${detainee}`);
-  return detainee;
-});
+const getDetaineeState = createSelector([getDetainee], (detainee) => detainee);
 
 const isDetaineeProfileLoadedState = createSelector(
   [isDetaineeProfileLoaded],
-  (loaded) => {
-    console.log(`selector isDetaineeProfileLoadedState: loaded = ${loaded}`);
-    return loaded;
-  },
+  (loaded) => loaded,
 );
 
 export default {
