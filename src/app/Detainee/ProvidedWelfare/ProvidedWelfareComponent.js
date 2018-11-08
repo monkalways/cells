45951cb0dd43e106592c2e3a861de654c51b5b67
Zachary Petriw <zachary.petriw@edmontonpolice.ca>
2018-11-07 18:05:30 -0700

    Add grid lines to Detainee Activity and Provided Welfare sections

--- src/app/Detainee/ProvidedWelfare/ProvidedWelfareComponent.js
+++ src/app/Detainee/ProvidedWelfare/ProvidedWelfareComponent.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  Avatar,
-  Divider,
-  Grid,
-  Typography,
-  withStyles,
+  Avatar, Grid, Typography, withStyles,
 } from '@material-ui/core';
 
 import VisualCheckIcon from '../../../images/WellnessVisual.png';
@@ -30,65 +26,103 @@ const propTypes = {
 const variant = 'subtitle2';
 
 const ProvidedWelfareComponent = ({ classes, detainee }) => (
-  <Grid container spacing={8} className={classes.grid}>
+  <Grid container className={classes.grid}>
     <Grid item xs={12}>
       <Typography variant="h6" className={classes.text}>
         Provided Welfare
       </Typography>
     </Grid>
     <Grid item xs={12}>
-      <Grid container spacing={16}>
-        <Grid item xs={1}>
+      <Grid container>
+        <Grid item xs={1} className={`${classes.gridItem} ${classes.topLeft}`}>
           <Avatar className={classes.avatar} src={VisualCheckIcon} />
         </Grid>
-        <Grid item xs={1}>
+        <Grid
+          item
+          xs={1}
+          className={`${classes.gridItem} ${classes.topCenter}`}
+        >
           <Avatar className={classes.avatar} src={VerbalCheckIcon} />
         </Grid>
-        <Grid item xs={1}>
+        <Grid
+          item
+          xs={1}
+          className={`${classes.gridItem} ${classes.topCenter}`}
+        >
           <Avatar className={classes.avatar} src={MealAcceptIcon} />
         </Grid>
-        <Grid item xs={1}>
+        <Grid
+          item
+          xs={1}
+          className={`${classes.gridItem} ${classes.topCenter}`}
+        >
           <Avatar className={classes.avatar} src={MealDeclineIcon} />
         </Grid>
-        <Grid item xs={1}>
+        <Grid
+          item
+          xs={1}
+          className={`${classes.gridItem} ${classes.topCenter}`}
+        >
           <Avatar className={classes.avatar} src={MedicineAcceptIcon} />
         </Grid>
-        <Grid item xs={1}>
+        <Grid item xs={1} className={`${classes.gridItem} ${classes.topRight}`}>
           <Avatar className={classes.avatar} src={MedicineDeclineIcon} />
         </Grid>
       </Grid>
     </Grid>
+
     <Grid item xs={12}>
-      <Divider />
-    </Grid>
-    <Grid item xs={12}>
-      <Grid container spacing={16}>
-        <Grid item xs={1}>
+      <Grid container>
+        <Grid
+          item
+          xs={1}
+          className={`${classes.gridItem} ${classes.bottomLeft}`}
+        >
           <Typography variant={variant} align="center">
             {detainee.visualCellCheckCount}
           </Typography>
         </Grid>
-        <Grid item xs={1}>
+        <Grid
+          item
+          xs={1}
+          className={`${classes.gridItem} ${classes.bottomCenter}`}
+        >
           <Typography variant={variant} align="center">
             {detainee.verbalCellCheckCount}
           </Typography>
         </Grid>
-        <Grid item xs={1}>
+        <Grid
+          item
+          xs={1}
+          className={`${classes.gridItem} ${classes.bottomCenter}`}
+        >
           <Typography variant={variant} align="center">
             {detainee.mealAcceptedCount}
           </Typography>
         </Grid>
-        <Grid item xs={1}>
+        <Grid
+          item
+          xs={1}
+          className={`${classes.gridItem} ${classes.bottomCenter}`}
+        >
           <Typography variant={variant} align="center">
             {detainee.mealDeclinedCount}
           </Typography>
         </Grid>
-        <Grid item xs={1}>
+        <Grid
+          item
+          xs={1}
+          className={`${classes.gridItem} ${classes.bottomCenter}`}
+        >
           <Typography variant={variant} align="center">
             {detainee.medicationAcceptedCount}
           </Typography>
         </Grid>
-        <Grid item xs={1}>
+        <Grid
+          item
+          xs={1}
+          className={`${classes.gridItem} ${classes.bottomRight}`}
+        >
           <Typography variant={variant} align="center">
             {detainee.medicationRefusedCount}
           </Typography>
@@ -104,10 +138,21 @@ export default withStyles((theme) => ({
     height: theme.spacing.unit * 6,
     width: theme.spacing.unit * 6,
   },
+  bottomLeft: { borderWidth: '1px 1px 0 0' },
+  bottomCenter: { borderWidth: '1px 1px 0 1px' },
+  bottomRight: { borderWidth: '1px 0 0 1px' },
   grid: {
+    padding: theme.spacing.unit * 0.5,
+  },
+  gridItem: {
+    borderStyle: 'solid',
+    borderColor: 'black',
     padding: theme.spacing.unit * 0.5,
   },
   text: {
     textDecorationLine: 'underline',
   },
+  topLeft: { borderWidth: '0 1px 1px 0' },
+  topCenter: { borderWidth: '0 1px 1px 1px' },
+  topRight: { borderWidth: '0 0 1px 1px' },
 }))(ProvidedWelfareComponent);
