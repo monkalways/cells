a888ed447e800721dc25e23bba18e3d52d056008
Zachary Petriw <zachary.petriw@edmontonpolice.ca>
2018-11-08 21:48:38 -0700

    Remove unnecessary container elements from PersonalStatsComponent

--- src/app/Detainee/PersonalInformation/PersonalStats/PersonalStatsComponent.js
+++ src/app/Detainee/PersonalInformation/PersonalStats/PersonalStatsComponent.js
@@ -9,64 +9,54 @@ const propTypes = {
 const PersonalStatsComponent = ({ detainee }) => (
   <React.Fragment>
     <Grid container spacing={8}>
-      <Grid item xs={12}>
-        <Grid container>
-          <Grid item xs={4}>
-            <Typography variant="body2">Name:</Typography>
-          </Grid>
-          <Grid item xs={8}>
-            <Typography variant="subtitle2">
-              {detainee.lastName}, {detainee.firstName}
-            </Typography>
-          </Grid>
+      <Grid container item xs={12}>
+        <Grid item xs={4}>
+          <Typography variant="body2">Name:</Typography>
+        </Grid>
+        <Grid item xs={8}>
+          <Typography variant="subtitle2">
+            {detainee.lastName}, {detainee.firstName}
+          </Typography>
         </Grid>
       </Grid>
 
-      <Grid item xs={12}>
-        <Grid container>
-          <Grid item xs={4}>
-            <Typography variant="body2">DOB:</Typography>
-          </Grid>
-          <Grid item xs={8}>
-            <Typography variant="subtitle2">{detainee.dob}</Typography>
-          </Grid>
+      <Grid container item xs={12}>
+        <Grid item xs={4}>
+          <Typography variant="body2">DOB:</Typography>
+        </Grid>
+        <Grid item xs={8}>
+          <Typography variant="subtitle2">{detainee.dob}</Typography>
         </Grid>
       </Grid>
 
-      <Grid item xs={12}>
-        <Grid container>
-          <Grid item xs={4}>
-            <Typography variant="body2">Gender:</Typography>
-          </Grid>
-          <Grid item xs={8}>
-            <Typography variant="subtitle2">{detainee.gender}</Typography>
-          </Grid>
+      <Grid container item xs={12}>
+        <Grid item xs={4}>
+          <Typography variant="body2">Gender:</Typography>
+        </Grid>
+        <Grid item xs={8}>
+          <Typography variant="subtitle2">{detainee.gender}</Typography>
         </Grid>
       </Grid>
 
-      <Grid item xs={12}>
-        <Grid container>
-          <Grid item xs={4}>
-            <Typography variant="body2">BIN:</Typography>
-          </Grid>
-          <Grid item xs={8}>
-            <Typography variant="subtitle2">
-              {detainee.propertyBagNumber ? detainee.propertyBagNumber : <br />}
-            </Typography>
-          </Grid>
+      <Grid container item xs={12}>
+        <Grid item xs={4}>
+          <Typography variant="body2">BIN:</Typography>
+        </Grid>
+        <Grid item xs={8}>
+          <Typography variant="subtitle2">
+            {detainee.propertyBagNumber ? detainee.propertyBagNumber : <br />}
+          </Typography>
         </Grid>
       </Grid>
 
-      <Grid item xs={12}>
-        <Grid container>
-          <Grid item xs={4}>
-            <Typography variant="body2">Remarks:</Typography>
-          </Grid>
-          <Grid item xs={8}>
-            <Typography variant="subtitle2">
-              {detainee.detentionUnitName}
-            </Typography>
-          </Grid>
+      <Grid container item xs={12}>
+        <Grid item xs={4}>
+          <Typography variant="body2">Remarks:</Typography>
+        </Grid>
+        <Grid item xs={8}>
+          <Typography variant="subtitle2">
+            {detainee.detentionUnitName}
+          </Typography>
         </Grid>
       </Grid>
     </Grid>
