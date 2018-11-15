f8f6c0cc49b77e0eda90c0ecd0f1f173d46761d6
Wei Wei <wei.wei@edmontonpolice.ca>
2018-11-15 10:52:57 -0700

    Add reducers.test.js

---
+++ src/app/ActivityRoom/duck/__tests__/reducers.test.js
@@ -0,0 +1,66 @@
+import activityRoomReducer from '../reducers';
+import types from '../types';
+import { types as authenticationTypes } from '../../../Authentication/duck';
+
+test('should setup default state', () => {
+  const result = activityRoomReducer(undefined, { type: '@@INIT' });
+  expect(result).toEqual({
+    usage: '',
+    loaded: false,
+    detainees: [],
+    checkingIn: false,
+  });
+});
+
+test('should update state by GET_ACTIVITY_ROOM_DETAINEES', () => {
+  const usage = 'phone';
+  const result = activityRoomReducer(undefined, {
+    type: types.GET_ACTIVITY_ROOM_DETAINEES,
+    usage,
+  });
+  expect(result.loaded).toBe(false);
+  expect(result.usage).toEqual(usage);
+});
+
+test('should update state by GET_ACTIVITY_ROOM_DETAINEES_SUCCESS', () => {
+  const detainees = [];
+  const result = activityRoomReducer(undefined, {
+    type: types.GET_ACTIVITY_ROOM_DETAINEES_SUCCESS,
+    detainees,
+  });
+  expect(result.loaded).toBe(true);
+  expect(result.detainees).toEqual(detainees);
+});
+
+test('should update state by CHECK_IN', () => {
+  const result = activityRoomReducer(undefined, {
+    type: types.CHECK_IN,
+  });
+  expect(result.checkingIn).toBe(true);
+});
+
+test('should update state by CHECK_IN_SUCCESS', () => {
+  const result = activityRoomReducer(undefined, {
+    type: types.CHECK_IN_SUCCESS,
+  });
+  expect(result.checkingIn).toBe(false);
+});
+
+test('should update state by CHECK_IN_FAIL', () => {
+  const result = activityRoomReducer(undefined, {
+    type: types.CHECK_IN_FAIL,
+  });
+  expect(result.checkingIn).toBe(false);
+});
+
+test('should update state by authentication action type LOG_OUT', () => {
+  const result = activityRoomReducer(undefined, {
+    type: authenticationTypes.LOG_OUT,
+  });
+  expect(result).toEqual({
+    usage: '',
+    loaded: false,
+    detainees: [],
+    checkingIn: false,
+  });
+});
