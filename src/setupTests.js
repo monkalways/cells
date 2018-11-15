fc8875cd9844a8b5da02cee3e6c8660ff402782f
Wei Wei <wei.wei@edmontonpolice.ca>
2018-11-15 14:02:43 -0700

    Add enzyme, jest-enzyme, and enzyme-adapter-react-16

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,8 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'jest-enzyme';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { configure } from 'enzyme';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
