b3a328a67880cff712567f87368a7bf086b2f496
Wei Wei <wei.wei@edmontonpolice.ca>
2018-11-16 14:02:28 -0700

    Add describe

--- src/app/ActivityRoom/duck/__tests__/actions.test.js
+++ src/app/ActivityRoom/duck/__tests__/actions.test.js
@@ -1,41 +1,43 @@
 import actions from '../actions';
 import types from '../types';
 
-it('should setup getActivityRoomDetainees action object', () => {
-  const usage = 'phone';
-  const action = actions.getActivityRoomDetainees(usage);
-  expect(action).toEqual({
-    type: types.GET_ACTIVITY_ROOM_DETAINEES,
-    usage,
+describe('ActivityRoom actions', () => {
+  it('should setup getActivityRoomDetainees action object', () => {
+    const usage = 'phone';
+    const action = actions.getActivityRoomDetainees(usage);
+    expect(action).toEqual({
+      type: types.GET_ACTIVITY_ROOM_DETAINEES,
+      usage,
+    });
   });
-});
 
-it('should setup getActivityRoomDetaineesSuccess action object', () => {
-  const detainees = [];
-  const action = actions.getActivityRoomDetaineesSuccess(detainees);
-  expect(action).toEqual({
-    type: types.GET_ACTIVITY_ROOM_DETAINEES_SUCCESS,
-    detainees,
+  it('should setup getActivityRoomDetaineesSuccess action object', () => {
+    const detainees = [];
+    const action = actions.getActivityRoomDetaineesSuccess(detainees);
+    expect(action).toEqual({
+      type: types.GET_ACTIVITY_ROOM_DETAINEES_SUCCESS,
+      detainees,
+    });
   });
-});
 
-it('should setup checkIn action object', () => {
-  const action = actions.checkIn();
-  expect(action).toEqual({
-    type: types.CHECK_IN,
+  it('should setup checkIn action object', () => {
+    const action = actions.checkIn();
+    expect(action).toEqual({
+      type: types.CHECK_IN,
+    });
   });
-});
 
-it('should setup checkInSuccess action object', () => {
-  const action = actions.checkInSuccess();
-  expect(action).toEqual({
-    type: types.CHECK_IN_SUCCESS,
+  it('should setup checkInSuccess action object', () => {
+    const action = actions.checkInSuccess();
+    expect(action).toEqual({
+      type: types.CHECK_IN_SUCCESS,
+    });
   });
-});
 
-it('should setup checkInFail action object', () => {
-  const action = actions.checkInFail();
-  expect(action).toEqual({
-    type: types.CHECK_IN_FAIL,
+  it('should setup checkInFail action object', () => {
+    const action = actions.checkInFail();
+    expect(action).toEqual({
+      type: types.CHECK_IN_FAIL,
+    });
   });
 });
