55aaf7c2893e24a24c83495a5c9208a4c8ba7fed
Wei Wei <wei.wei@edmontonpolice.ca>
2018-11-20 12:52:05 -0700

    Add CellDetaineeCardComponent.test.js

---
+++ src/app/Cell/Meal/CellDetaineeCard/__tests__/CellDetaineeCardComponent.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { CellDetaineeCardComponent } from '../CellDetaineeCardComponent';
+
+describe('CellDetaineeCardComponent', () => {
+  const setup = () => {
+    const classes = {
+      card: 'card',
+    };
+    return shallow(<CellDetaineeCardComponent
+      classes={classes}
+      cellDetainee={{}}
+      cellName="c1"
+      isAuthenticated={false}
+      cellCheck={{}}
+      onAcceptClick={jest.fn()}
+      onRejectClick={jest.fn()}
+      onNotApplicableClick={jest.fn()}
+    />);
+  };
+
+  it('should render', () => {
+    const wrapper = setup();
+    expect(wrapper).toMatchSnapshot();
+  });
+});
