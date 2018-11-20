b1162d428c99ff047c7d2bc3f5cb8b6a2ebe3634
Wei Wei <wei.wei@edmontonpolice.ca>
2018-11-20 13:17:36 -0700

    Regroup test cases

--- src/app/Cell/CellCheck/__tests__/CellCheckComponent.test.js
+++ src/app/Cell/CellCheck/__tests__/CellCheckComponent.test.js
@@ -97,345 +97,351 @@ describe('CellCheckComponent', () => {
     expect(getCellDetainees).toBeCalledWith(cellName);
   });
 
-  it('should return empty string if cell detainees are not loaded', () => {
-    isCellDetaineesLoaded = false;
+  describe('getCellCheckRadioButtonValue', () => {
+    it('should return empty string if cell detainees are not loaded', () => {
+      isCellDetaineesLoaded = false;
 
-    const wrapper = setup();
-    const result = wrapper.instance().getCellCheckRadioButtonValue();
+      const wrapper = setup();
+      const result = wrapper.instance().getCellCheckRadioButtonValue();
 
-    expect(result).toEqual('');
+      expect(result).toEqual('');
+    });
+
+    it('should return empty string if cell check data is empty', () => {
+      isCellDetaineesLoaded = true;
+      cellCheck = {};
+
+      const wrapper = setup();
+      const result = wrapper.instance().getCellCheckRadioButtonValue();
+
+      expect(result).toEqual('');
+    });
+
+    it('should return "visual" if all cell checks of in-cell detainees are "visually checked"', () => {
+      isCellDetaineesLoaded = true;
+      cellDetainees = [
+        {
+          id: '123',
+          location: null,
+        },
+        {
+          id: '456',
+          location: null,
+        },
+        {
+          id: '789',
+          location: 'Phone - In Transit',
+        },
+      ];
+      cellCheck = {
+        123: {
+          visual: true,
+          verbal: false,
+        },
+        456: {
+          visual: true,
+          verbal: false,
+        },
+        789: {
+          visual: false,
+          verbal: true,
+        },
+      };
+      const wrapper = setup();
+      const result = wrapper.instance().getCellCheckRadioButtonValue();
+
+      expect(result).toEqual('visual');
+    });
+
+    it('should return "verbal" if all cell checks of in-cell detainees are "verbally checked"', () => {
+      isCellDetaineesLoaded = true;
+      cellDetainees = [
+        {
+          id: '123',
+          location: null,
+        },
+        {
+          id: '456',
+          location: null,
+        },
+        {
+          id: '789',
+          location: 'Phone - In Transit',
+        },
+      ];
+      cellCheck = {
+        123: {
+          visual: false,
+          verbal: true,
+        },
+        456: {
+          visual: false,
+          verbal: true,
+        },
+        789: {
+          visual: false,
+          verbal: true,
+        },
+      };
+      const wrapper = setup();
+      const result = wrapper.instance().getCellCheckRadioButtonValue();
+
+      expect(result).toEqual('verbal');
+    });
+
+    it('should return empty if some in-cell detainees are "verbally checked" and others are "visually checked"', () => {
+      isCellDetaineesLoaded = true;
+      cellDetainees = [
+        {
+          id: '123',
+          location: null,
+        },
+        {
+          id: '456',
+          location: null,
+        },
+        {
+          id: '789',
+          location: 'Phone - In Transit',
+        },
+      ];
+      cellCheck = {
+        123: {
+          visual: true,
+          verbal: false,
+        },
+        456: {
+          visual: false,
+          verbal: true,
+        },
+        789: {
+          visual: false,
+          verbal: true,
+        },
+      };
+      const wrapper = setup();
+      const result = wrapper.instance().getCellCheckRadioButtonValue();
+
+      expect(result).toEqual('');
+    });
+
+    it('should return empty if some in-cell detainees are "verbally checked" and others are "visually checked"', () => {
+      isCellDetaineesLoaded = true;
+      cellDetainees = [
+        {
+          id: '123',
+          location: null,
+        },
+        {
+          id: '456',
+          location: null,
+        },
+        {
+          id: '789',
+          location: 'Phone - In Transit',
+        },
+      ];
+      cellCheck = {
+        123: {
+          visual: true,
+          verbal: false,
+        },
+        456: {
+          visual: false,
+          verbal: true,
+        },
+        789: {
+          visual: false,
+          verbal: true,
+        },
+      };
+      const wrapper = setup();
+      const result = wrapper.instance().getCellCheckRadioButtonValue();
+
+      expect(result).toEqual('');
+    });
   });
 
-  it('should return empty string if cell check data is empty', () => {
-    isCellDetaineesLoaded = true;
-    cellCheck = {};
+  describe('isSaveDisabled', () => {
+    it('should disable save if cell detainees are not loaded', () => {
+      isCellDetaineesLoaded = false;
+      const wrapper = setup();
 
-    const wrapper = setup();
-    const result = wrapper.instance().getCellCheckRadioButtonValue();
+      const result = wrapper.instance().isSaveDisabled();
 
-    expect(result).toEqual('');
+      expect(result).toBe(true);
+    });
+
+    it('should disable save if cell check data is empty', () => {
+      isCellDetaineesLoaded = true;
+      cellDetainees = [
+        {
+          id: '123',
+          location: null,
+        },
+      ];
+      cellCheck = {};
+      const wrapper = setup();
+
+      const result = wrapper.instance().isSaveDisabled();
+
+      expect(result).toBe(true);
+    });
+
+    it('should disable save if cell detainees data is empty', () => {
+      isCellDetaineesLoaded = true;
+      cellDetainees = [];
+      cellCheck = {
+        123: {
+          visual: true,
+          verbal: false,
+        },
+      };
+      const wrapper = setup();
+
+      const result = wrapper.instance().isSaveDisabled();
+
+      expect(result).toBe(true);
+    });
+
+    it('should disable save if all cell detainees are not in cell', () => {
+      isCellDetaineesLoaded = true;
+      cellDetainees = [
+        {
+          id: '123',
+          location: 'Phone - In Transit',
+        },
+        {
+          id: '456',
+          location: 'Phone - In Transit',
+        },
+      ];
+      cellCheck = {
+        123: {
+          visual: false,
+          verbal: false,
+        },
+        456: {
+          visual: false,
+          verbal: false,
+        },
+      };
+      const wrapper = setup();
+
+      const result = wrapper.instance().isSaveDisabled();
+
+      expect(result).toBe(true);
+    });
+
+    it('should enable save if some cell detainees are in cell', () => {
+      isCellDetaineesLoaded = true;
+      cellDetainees = [
+        {
+          id: '123',
+          location: 'Phone - In Transit',
+        },
+        {
+          id: '456',
+          location: null,
+        },
+      ];
+      cellCheck = {
+        123: {
+          visual: false,
+          verbal: false,
+        },
+        456: {
+          visual: true,
+          verbal: false,
+        },
+      };
+      const wrapper = setup();
+
+      const result = wrapper.instance().isSaveDisabled();
+
+      expect(result).toBe(false);
+    });
   });
 
-  it('should return "visual" if all cell checks of in-cell detainees are "visually checked"', () => {
-    isCellDetaineesLoaded = true;
-    cellDetainees = [
-      {
-        id: '123',
-        location: null,
-      },
-      {
-        id: '456',
-        location: null,
-      },
-      {
-        id: '789',
-        location: 'Phone - In Transit',
-      },
-    ];
-    cellCheck = {
-      123: {
-        visual: true,
-        verbal: false,
-      },
-      456: {
-        visual: true,
-        verbal: false,
-      },
-      789: {
-        visual: false,
-        verbal: true,
-      },
-    };
-    const wrapper = setup();
-    const result = wrapper.instance().getCellCheckRadioButtonValue();
+  describe('handleRadioGroupChange', () => {
+    it('should visual check all in-cell detainees if radio button group value is changed to "visual"', () => {
+      cellDetainees = [
+        {
+          id: '123',
+          location: 'Phone - In Transit',
+        },
+        {
+          id: '456',
+          location: null,
+        },
+      ];
+      const event = {
+        target: {
+          value: 'visual',
+        },
+      };
+      const wrapper = setup();
+      wrapper.instance().handleRadioGroupChange(event);
 
-    expect(result).toEqual('visual');
-  });
+      expect(visualCheckAll).toBeCalledWith([
+        {
+          id: '456',
+          location: null,
+        },
+      ]);
+      expect(verbalCheckAll).not.toBeCalled();
+    });
 
-  it('should return "verbal" if all cell checks of in-cell detainees are "verbally checked"', () => {
-    isCellDetaineesLoaded = true;
-    cellDetainees = [
-      {
-        id: '123',
-        location: null,
-      },
-      {
-        id: '456',
-        location: null,
-      },
-      {
-        id: '789',
-        location: 'Phone - In Transit',
-      },
-    ];
-    cellCheck = {
-      123: {
-        visual: false,
-        verbal: true,
-      },
-      456: {
-        visual: false,
-        verbal: true,
-      },
-      789: {
-        visual: false,
-        verbal: true,
-      },
-    };
-    const wrapper = setup();
-    const result = wrapper.instance().getCellCheckRadioButtonValue();
+    it('should verbal check all in-cell detainees if radio button group value is changed to "verbal"', () => {
+      cellDetainees = [
+        {
+          id: '123',
+          location: 'Phone - In Transit',
+        },
+        {
+          id: '456',
+          location: null,
+        },
+      ];
+      const event = {
+        target: {
+          value: 'verbal',
+        },
+      };
+      const wrapper = setup();
+      wrapper.instance().handleRadioGroupChange(event);
 
-    expect(result).toEqual('verbal');
-  });
+      expect(verbalCheckAll).toBeCalledWith([
+        {
+          id: '456',
+          location: null,
+        },
+      ]);
+      expect(visualCheckAll).not.toBeCalled();
+    });
 
-  it('should return empty if some in-cell detainees are "verbally checked" and others are "visually checked', () => {
-    isCellDetaineesLoaded = true;
-    cellDetainees = [
-      {
-        id: '123',
-        location: null,
-      },
-      {
-        id: '456',
-        location: null,
-      },
-      {
-        id: '789',
-        location: 'Phone - In Transit',
-      },
-    ];
-    cellCheck = {
-      123: {
-        visual: true,
-        verbal: false,
-      },
-      456: {
-        visual: false,
-        verbal: true,
-      },
-      789: {
-        visual: false,
-        verbal: true,
-      },
-    };
-    const wrapper = setup();
-    const result = wrapper.instance().getCellCheckRadioButtonValue();
+    it('should not cell check any in-cell detainees if radio button group value is changed to other values than "visual" and "verbal"', () => {
+      cellDetainees = [
+        {
+          id: '123',
+          location: 'Phone - In Transit',
+        },
+        {
+          id: '456',
+          location: null,
+        },
+      ];
+      const event = {
+        target: {
+          value: 'other',
+        },
+      };
+      const wrapper = setup();
+      wrapper.instance().handleRadioGroupChange(event);
 
-    expect(result).toEqual('');
-  });
-
-  it('should return empty if some in-cell detainees are "verbally checked" and others are "visually checked', () => {
-    isCellDetaineesLoaded = true;
-    cellDetainees = [
-      {
-        id: '123',
-        location: null,
-      },
-      {
-        id: '456',
-        location: null,
-      },
-      {
-        id: '789',
-        location: 'Phone - In Transit',
-      },
-    ];
-    cellCheck = {
-      123: {
-        visual: true,
-        verbal: false,
-      },
-      456: {
-        visual: false,
-        verbal: true,
-      },
-      789: {
-        visual: false,
-        verbal: true,
-      },
-    };
-    const wrapper = setup();
-    const result = wrapper.instance().getCellCheckRadioButtonValue();
-
-    expect(result).toEqual('');
-  });
-
-  it('should disable save if cell detainees are not loaded', () => {
-    isCellDetaineesLoaded = false;
-    const wrapper = setup();
-
-    const result = wrapper.instance().isSaveDisabled();
-
-    expect(result).toBe(true);
-  });
-
-  it('should disable save if cell check data is empty', () => {
-    isCellDetaineesLoaded = true;
-    cellDetainees = [
-      {
-        id: '123',
-        location: null,
-      },
-    ];
-    cellCheck = {};
-    const wrapper = setup();
-
-    const result = wrapper.instance().isSaveDisabled();
-
-    expect(result).toBe(true);
-  });
-
-  it('should disable save if cell detainees data is empty', () => {
-    isCellDetaineesLoaded = true;
-    cellDetainees = [];
-    cellCheck = {
-      123: {
-        visual: true,
-        verbal: false,
-      },
-    };
-    const wrapper = setup();
-
-    const result = wrapper.instance().isSaveDisabled();
-
-    expect(result).toBe(true);
-  });
-
-  it('should disable save if all cell detainees are not in cell', () => {
-    isCellDetaineesLoaded = true;
-    cellDetainees = [
-      {
-        id: '123',
-        location: 'Phone - In Transit',
-      },
-      {
-        id: '456',
-        location: 'Phone - In Transit',
-      },
-    ];
-    cellCheck = {
-      123: {
-        visual: false,
-        verbal: false,
-      },
-      456: {
-        visual: false,
-        verbal: false,
-      },
-    };
-    const wrapper = setup();
-
-    const result = wrapper.instance().isSaveDisabled();
-
-    expect(result).toBe(true);
-  });
-
-  it('should enable save if some cell detainees are in cell', () => {
-    isCellDetaineesLoaded = true;
-    cellDetainees = [
-      {
-        id: '123',
-        location: 'Phone - In Transit',
-      },
-      {
-        id: '456',
-        location: null,
-      },
-    ];
-    cellCheck = {
-      123: {
-        visual: false,
-        verbal: false,
-      },
-      456: {
-        visual: true,
-        verbal: false,
-      },
-    };
-    const wrapper = setup();
-
-    const result = wrapper.instance().isSaveDisabled();
-
-    expect(result).toBe(false);
-  });
-
-  it('should visual check all in-cell detainees if radio button group value is changed to "visual"', () => {
-    cellDetainees = [
-      {
-        id: '123',
-        location: 'Phone - In Transit',
-      },
-      {
-        id: '456',
-        location: null,
-      },
-    ];
-    const event = {
-      target: {
-        value: 'visual',
-      },
-    };
-    const wrapper = setup();
-    wrapper.instance().handleRadioGroupChange(event);
-
-    expect(visualCheckAll).toBeCalledWith([
-      {
-        id: '456',
-        location: null,
-      },
-    ]);
-    expect(verbalCheckAll).not.toBeCalled();
-  });
-
-  it('should verbal check all in-cell detainees if radio button group value is changed to "verbal"', () => {
-    cellDetainees = [
-      {
-        id: '123',
-        location: 'Phone - In Transit',
-      },
-      {
-        id: '456',
-        location: null,
-      },
-    ];
-    const event = {
-      target: {
-        value: 'verbal',
-      },
-    };
-    const wrapper = setup();
-    wrapper.instance().handleRadioGroupChange(event);
-
-    expect(verbalCheckAll).toBeCalledWith([
-      {
-        id: '456',
-        location: null,
-      },
-    ]);
-    expect(visualCheckAll).not.toBeCalled();
-  });
-
-  it('should not cell check any in-cell detainees if radio button group value is changed to other values than "visual" and "verbal"', () => {
-    cellDetainees = [
-      {
-        id: '123',
-        location: 'Phone - In Transit',
-      },
-      {
-        id: '456',
-        location: null,
-      },
-    ];
-    const event = {
-      target: {
-        value: 'other',
-      },
-    };
-    const wrapper = setup();
-    wrapper.instance().handleRadioGroupChange(event);
-
-    expect(visualCheckAll).not.toBeCalled();
-    expect(verbalCheckAll).not.toBeCalled();
+      expect(visualCheckAll).not.toBeCalled();
+      expect(verbalCheckAll).not.toBeCalled();
+    });
   });
 
   it('should handle save', () => {
