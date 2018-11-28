78d8b5f365180548de96df2ae4ec042471675b65
Wei Wei <wei.wei@edmontonpolice.ca>
2018-11-28 08:54:04 -0700

    Add default route

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import routes from './routes';
+import Home from './Home';
 
 const App = () => (
   <React.Fragment>
@@ -8,6 +9,7 @@ const App = () => (
       {routes.map((route) => (
         <Route key={route.path} {...route} />
       ))}
+      <Route component={Home} />
     </Switch>
   </React.Fragment>
 );
