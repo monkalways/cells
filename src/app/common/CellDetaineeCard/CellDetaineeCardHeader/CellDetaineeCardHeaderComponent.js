a0b31390d8de61bc74cb8afa797c5f9318b30ea0
Wei Wei <wei.wei@edmontonpolice.ca>
2018-11-30 14:03:01 -0700

    Use LocationIcon for rendering location icon

--- src/app/common/CellDetaineeCard/CellDetaineeCardHeader/CellDetaineeCardHeaderComponent.js
+++ src/app/common/CellDetaineeCard/CellDetaineeCardHeader/CellDetaineeCardHeaderComponent.js
@@ -8,18 +8,7 @@ import {
   withStyles,
 } from '@material-ui/core';
 
-import InCellIcon from '../../../images/InCell.png';
-import InTransitIcon from '../../../images/InTransit.png';
-import BailHearingGeneralIcon from '../../../images/BailHearingGeneral.png';
-import FingerprintingIcon from '../../../images/Fingerprinting.png';
-import MedicalVisitIcon from '../../../images/MedicalVisit.png';
-import PhoneAcceptIcon from '../../../images/PhoneAccept.png';
-
-const LOCATION_PHONE_ACCEPT = 'phone - in progress';
-const LOCATION_MEDICAL_VISIT = 'medical - in progress';
-const LOCATION_FINGER_PRINTING = 'fingerprinting - in progress';
-const LOCATION_BAIL_HEARING = 'bail hearing - in progress';
-const LOCATION_IN_TRANSIT = 'in transit';
+import LocationIcon from '../../LocationIcon';
 
 const propTypes = {
   classes: PropTypes.shape({}).isRequired,
@@ -31,58 +20,26 @@ const propTypes = {
   }).isRequired,
 };
 
-export const CellDetaineeCardHeaderComponent = ({ classes, cellDetainee }) => {
-  const getLocationIconSrc = () => {
-    const { location } = cellDetainee;
-    let locationIconSrc = InCellIcon;
-
-    if (location) {
-      if (location.toLowerCase().includes(LOCATION_IN_TRANSIT)) {
-        locationIconSrc = InTransitIcon;
-      }
-
-      switch (location.toLowerCase()) {
-        case LOCATION_PHONE_ACCEPT:
-          locationIconSrc = PhoneAcceptIcon;
-          break;
-        case LOCATION_MEDICAL_VISIT:
-          locationIconSrc = MedicalVisitIcon;
-          break;
-        case LOCATION_FINGER_PRINTING:
-          locationIconSrc = FingerprintingIcon;
-          break;
-        case LOCATION_BAIL_HEARING:
-          locationIconSrc = BailHearingGeneralIcon;
-          break;
-        default:
-          break;
-      }
-    }
-
-    return locationIconSrc;
-  };
-
-  return (
-    <CardContent className={classes.header}>
-      <Grid container>
-        <Grid item sm={7}>
-          <Typography variant="h6" className={classes.heading}>
-            {cellDetainee.lastName}
-          </Typography>
-          <Typography variant="h6" className={classes.heading}>
-            {cellDetainee.firstName}
-          </Typography>
-          <Typography variant="h6" className={classes.subtitle}>
-            {`${cellDetainee.division.join(',')}`}
-          </Typography>
-        </Grid>
-        <Grid item sm={5}>
-          <Avatar className={classes.avatar} src={getLocationIconSrc()} />
-        </Grid>
+export const CellDetaineeCardHeaderComponent = ({ classes, cellDetainee }) => (
+  <CardContent className={classes.header}>
+    <Grid container>
+      <Grid item sm={7}>
+        <Typography variant="h6" className={classes.heading}>
+          {cellDetainee.lastName}
+        </Typography>
+        <Typography variant="h6" className={classes.heading}>
+          {cellDetainee.firstName}
+        </Typography>
+        <Typography variant="h6" className={classes.subtitle}>
+          {`${cellDetainee.division.join(',')}`}
+        </Typography>
       </Grid>
-    </CardContent>
-  );
-};
+      <Grid item sm={5}>
+        <LocationIcon location={cellDetainee.location} />
+      </Grid>
+    </Grid>
+  </CardContent>
+);
 
 CellDetaineeCardHeaderComponent.propTypes = propTypes;
 
@@ -97,10 +54,5 @@ export default withStyles((theme) => ({
   },
   subtitle: {
     color: '#7B2C31',
-  },
-  avatar: {
-    margin: theme.spacing.unit,
-    height: theme.spacing.unit * 10,
-    width: theme.spacing.unit * 10,
   },
 }))(CellDetaineeCardHeaderComponent);
--- src/app/common/CellDetaineeCard/CellDetaineeCardHeader/__tests__/CellDetaineeCardHeaderComponent.test.js
+++ src/app/common/CellDetaineeCard/CellDetaineeCardHeader/__tests__/CellDetaineeCardHeaderComponent.test.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { Avatar } from '@material-ui/core';
 import { CellDetaineeCardHeaderComponent } from '../CellDetaineeCardHeaderComponent';
-
-import InCellIcon from '../../../../images/InCell.png';
-import InTransitIcon from '../../../../images/InTransit.png';
-import BailHearingGeneralIcon from '../../../../images/BailHearingGeneral.png';
-import FingerprintingIcon from '../../../../images/Fingerprinting.png';
-import MedicalVisitIcon from '../../../../images/MedicalVisit.png';
-import PhoneAcceptIcon from '../../../../images/PhoneAccept.png';
 
 describe('CellDetaineeCardHeaderComponent', () => {
   let classes;
@@ -38,25 +30,4 @@ describe('CellDetaineeCardHeaderComponent', () => {
     const wrapper = setup();
     expect(wrapper).toMatchSnapshot();
   });
-
-  it.each([
-    ['phone - in progress', PhoneAcceptIcon],
-    ['medical - in progress', MedicalVisitIcon],
-    ['fingerprinting - in progress', FingerprintingIcon],
-    ['bail hearing - in progress', BailHearingGeneralIcon],
-    ['phone - in transit', InTransitIcon],
-    ['medical - in transit', InTransitIcon],
-    ['fingerprinting - in transit', InTransitIcon],
-    ['bail hearing - in transit', InTransitIcon],
-    ['other', InCellIcon],
-    [null, InCellIcon],
-  ])(
-    'should render avatar based on cell detainee location',
-    (location, avatarSrc) => {
-      cellDetainee.location = location;
-      const wrapper = setup();
-
-      expect(wrapper.find(Avatar)).toHaveProp('src', avatarSrc);
-    },
-  );
 });
