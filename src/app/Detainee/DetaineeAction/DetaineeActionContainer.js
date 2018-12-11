5560328f9c2e81127c76be9184b5f16b4c14b144
Zachary Petriw <zachary.petriw@edmontonpolice.ca>
2018-12-11 10:29:54 -0700

    Refactor DetaineeActionContainer constants to use common constants

--- src/app/Detainee/DetaineeAction/DetaineeActionContainer.js
+++ src/app/Detainee/DetaineeAction/DetaineeActionContainer.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import { operations, selectors } from '../duck';
 import constants from '../constants';
+import commonConstants from '../../constants';
 import DetaineeActionComponent from './DetaineeActionComponent';
 
 export const mapStateToProps = (
@@ -8,58 +9,58 @@ export const mapStateToProps = (
   isBailHearingRoom1OptionAvailable = selectors.isActivityRoomOptionAvailableState(
     state,
     constants.BAIL_HEARING_ROOM_1,
-    constants.BAIL_HEARING_IN_PROGRESS,
-    constants.BAIL_HEARING_IN_TRANSIT,
+    commonConstants.BAIL_HEARING_IN_PROGRESS,
+    commonConstants.BAIL_HEARING_IN_TRANSIT,
   ),
   isBailHearingRoom2OptionAvailable = selectors.isActivityRoomOptionAvailableState(
     state,
     constants.BAIL_HEARING_ROOM_2,
-    constants.BAIL_HEARING_IN_PROGRESS,
-    constants.BAIL_HEARING_IN_TRANSIT,
+    commonConstants.BAIL_HEARING_IN_PROGRESS,
+    commonConstants.BAIL_HEARING_IN_TRANSIT,
   ),
   isBreathTestRoomOptionAvailable = selectors.isActivityRoomOptionAvailableState(
     state,
     constants.BREATH_TEST_ROOM,
-    constants.BREATH_TEST_IN_PROGRESS,
-    constants.BREATH_TEST_IN_TRANSIT,
+    commonConstants.BREATH_TEST_IN_PROGRESS,
+    commonConstants.BREATH_TEST_IN_TRANSIT,
   ),
   isFingerprintingRoomOptionAvailable = selectors.isActivityRoomOptionAvailableState(
     state,
     constants.FINGERPRINTING_ROOM,
-    constants.FINGERPRINTING_IN_PROGRESS,
-    constants.FINGERPRINTING_IN_TRANSIT,
+    commonConstants.FINGERPRINTING_IN_PROGRESS,
+    commonConstants.FINGERPRINTING_IN_TRANSIT,
   ),
   isInCellOptionAvailable = selectors.isInCellOptionAvailableState(state),
   isInterviewRoomOptionAvailable = selectors.isActivityRoomOptionAvailableState(
     state,
     constants.INTERVIEW_ROOM,
-    constants.INTERVIEW_IN_PROGRESS,
-    constants.INTERVIEW_IN_TRANSIT,
+    commonConstants.INTERVIEW_IN_PROGRESS,
+    commonConstants.INTERVIEW_IN_TRANSIT,
   ),
   isMedicalRoomOptionAvailable = selectors.isActivityRoomOptionAvailableState(
     state,
     constants.MEDICAL_ROOM,
-    constants.MEDICAL_IN_PROGRESS,
-    constants.MEDICAL_IN_TRANSIT,
+    commonConstants.MEDICAL_IN_PROGRESS,
+    commonConstants.MEDICAL_IN_TRANSIT,
   ),
   isPhoneDeclineOptionAvailable = selectors.isPhoneDeclineOptionAvailableState(state),
   isPhoneRoomOptionAvailable = selectors.isActivityRoomOptionAvailableState(
     state,
     constants.PHONE_ROOM,
-    constants.PHONE_IN_PROGRESS,
-    constants.PHONE_IN_TRANSIT,
+    commonConstants.PHONE_IN_PROGRESS,
+    commonConstants.PHONE_IN_TRANSIT,
   ),
   isReleaseRoomOptionAvailable = selectors.isActivityRoomOptionAvailableState(
     state,
     constants.RELEASE_ROOM,
     '',
-    constants.RELEASE_HOLDING_IN_TRANSIT,
+    commonConstants.RELEASE_HOLDING_IN_TRANSIT,
   ),
   isRemandHoldingRoomOptionAvailable = selectors.isActivityRoomOptionAvailableState(
     state,
     constants.REMAND_HOLDING_ROOM,
     '',
-    constants.RELEASE_HOLDING_IN_TRANSIT,
+    commonConstants.RELEASE_HOLDING_IN_TRANSIT,
   ),
 ) => ({
   isBailHearingRoom1OptionAvailable,
