b141d075e477837dacfc466ba0c5b46550590773
Zachary Petriw <zachary.petriw@edmontonpolice.ca>
2019-01-09 12:55:43 -0700

    Add Release and Remand dialog containers with tests

---
+++ src/app/Detainee/DetaineeAction/RemandRoomDialog/RemandRoomDialogContainer.js
@@ -0,0 +1,39 @@
+import { connect } from 'react-redux';
+import { operations, selectors } from '../../duck';
+import AutomaticRoomSelectionDialog from '../AutomaticRoomSelectionDialog';
+import constants from '../../constants';
+
+export const mapStateToProps = (
+  state,
+  areRoomsRefreshing = false, // *** fill this in
+  currentRoom = selectors.getCurrentRoomState(state),
+  destinationRoom = selectors.getFirstAvailableRemandRoomState(state),
+  isAnyRoomAvailable = selectors.isRemandRoomOptionAvailableState(state),
+  isAssigningToRoom = false, // *** fill this in
+  usage = constants.RELEASE_ROOM,
+) => ({
+  areRoomsRefreshing,
+  currentRoom,
+  destinationRoom,
+  isAnyRoomAvailable,
+  isAssigningToRoom,
+  usage,
+});
+
+export const mapDispatchToProps = (dispatch) => ({
+  // eslint-disable-next-line max-len
+  getAvailableRoomsRefresh: (getAvailableRoomsRefresh = operations.getAvailableRemandRoomsRefresh) => {
+    dispatch(getAvailableRoomsRefresh());
+  },
+  moveDetaineeToRoom: (
+    detaineeId,
+    originRoom,
+    destinationRoom,
+    moveDetaineeToActivityRoom = operations.moveDetaineeToRoom,
+  ) => dispatch(moveDetaineeToActivityRoom(detaineeId, originRoom, destinationRoom)),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(AutomaticRoomSelectionDialog);
---
+++ src/app/Detainee/DetaineeAction/RemandRoomDialog/__tests__/RemandRoomDialogContainer.test.js
@@ -0,0 +1,75 @@
+import {
+  mapStateToProps,
+  mapDispatchToProps,
+} from '../RemandRoomDialogContainer';
+
+describe('RemandRoomDialogContainer', () => {
+  it('should mapStateToProps', () => {
+    const state = {};
+    const areRoomsRefreshing = false;
+    const currentRoom = 'C1';
+    const destinationRoom = 'T2';
+    const isAnyRoomAvailable = false;
+    const isAssigningToRoom = false;
+    const usage = 'REMAND HOLDING';
+
+    const result = mapStateToProps(
+      state,
+      areRoomsRefreshing,
+      currentRoom,
+      destinationRoom,
+      isAnyRoomAvailable,
+      isAssigningToRoom,
+      usage,
+    );
+
+    expect(result).toEqual({
+      areRoomsRefreshing,
+      currentRoom,
+      destinationRoom,
+      isAnyRoomAvailable,
+      isAssigningToRoom,
+      usage,
+    });
+  });
+
+  describe('mapDispatchToProps', () => {
+    let dispatch;
+
+    beforeEach(() => {
+      dispatch = jest.fn();
+    });
+
+    it('should getAvailableActivityRooms', () => {
+      const { getAvailableRoomsRefresh } = mapDispatchToProps(dispatch);
+      const getAvailableActivityRoomsRefreshMock = jest.fn();
+
+      getAvailableRoomsRefresh(getAvailableActivityRoomsRefreshMock);
+
+      expect(dispatch).toBeCalled();
+      expect(getAvailableActivityRoomsRefreshMock).toBeCalled();
+    });
+
+    it('should moveDetaineeToActivityRoom', () => {
+      const { moveDetaineeToRoom } = mapDispatchToProps(dispatch);
+      const detaineeId = '123';
+      const originRoom = 'C1';
+      const destinationRoom = 'T4';
+      const moveDetaineeToActivityRoomMock = jest.fn();
+
+      moveDetaineeToRoom(
+        detaineeId,
+        originRoom,
+        destinationRoom,
+        moveDetaineeToActivityRoomMock,
+      );
+
+      expect(dispatch).toBeCalled();
+      expect(moveDetaineeToActivityRoomMock).toBeCalledWith(
+        detaineeId,
+        originRoom,
+        destinationRoom,
+      );
+    });
+  });
+});
