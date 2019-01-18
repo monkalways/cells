121b1a876bfea23f76135b36b0d3472283b9f1e5
Zachary Petriw <zachary.petriw@edmontonpolice.ca>
2019-01-18 11:38:29 -0700

    Add authenticationTimeoutReducer to common reducers and update tests

--- src/app/common/duck/__tests__/reducers.test.js
+++ src/app/common/duck/__tests__/reducers.test.js
@@ -1,34 +1,69 @@
 import types from '../types';
-import { versionsReducer } from '../reducers';
+import { authenticationTimeoutReducer, versionsReducer } from '../reducers';
 
-describe('versionsReducer', () => {
-  it('should setup default state', () => {
-    const defaultState = {
-      data: [],
-    };
-    const result = versionsReducer(undefined, { type: '@@INIT' });
-    expect(result).toEqual(defaultState);
+describe('Common reudcers', () => {
+  describe('authenticationTimeoutReducer', () => {
+    it('should set up default state', () => {
+      const defaultState = {
+        timeout: null,
+      };
+      const result = authenticationTimeoutReducer(undefined, {
+        types: '@@INIT',
+      });
+      expect(result).toEqual(defaultState);
+    });
   });
 
-  it('should update state by GET_VERSIONS_SUCCESS', () => {
-    const payload = [
-      {
-        name: 'Cell App',
-        version: '1.0.0',
-      },
-      {
-        name: 'Detainee Management Service',
-        version: '1.0.0',
-      },
-      {
-        name: 'EPROS Service',
-        version: '1.0.0',
-      },
-    ];
-    const result = versionsReducer(undefined, {
-      type: types.GET_VERSIONS_SUCCESS,
-      payload,
+  it('should refresh authentication timeout', () => {
+    const result = authenticationTimeoutReducer(undefined, {
+      type: types.REFRESH_AUTHENTICATION_TIMEOUT,
     });
-    expect(result.data).toEqual(payload);
+    expect(result.timeout).not.toBeNull();
+  });
+
+  it('should start authentication timeout', () => {
+    const result = authenticationTimeoutReducer(undefined, {
+      type: types.START_AUTHENTICATION_TIMEOUT,
+    });
+    expect(result.timeout).not.toBeNull();
+  });
+
+  it('should stop authentication timeout', () => {
+    const result = authenticationTimeoutReducer(undefined, {
+      type: types.STOP_AUTHENTICATION_TIMEOUT,
+    });
+    expect(result.timeout).toBeNull();
+  });
+
+  describe('versionsReducer', () => {
+    it('should set up default state', () => {
+      const defaultState = {
+        data: [],
+      };
+      const result = versionsReducer(undefined, { type: '@@INIT' });
+      expect(result).toEqual(defaultState);
+    });
+
+    it('should update state by GET_VERSIONS_SUCCESS', () => {
+      const payload = [
+        {
+          name: 'Cell App',
+          version: '1.0.0',
+        },
+        {
+          name: 'Detainee Management Service',
+          version: '1.0.0',
+        },
+        {
+          name: 'EPROS Service',
+          version: '1.0.0',
+        },
+      ];
+      const result = versionsReducer(undefined, {
+        type: types.GET_VERSIONS_SUCCESS,
+        payload,
+      });
+      expect(result.data).toEqual(payload);
+    });
   });
 });
--- src/app/common/duck/reducers.js
+++ src/app/common/duck/reducers.js
@@ -1,6 +1,46 @@
 import { combineReducers } from 'redux';
 import types from './types';
 
+const defaultAuthenticationTimeoutReducerState = {
+  timeout: null,
+};
+
+export const authenticationTimeoutReducer = (
+  state = defaultAuthenticationTimeoutReducerState,
+  action,
+) => {
+  switch (action.type) {
+    case types.REFRESH_AUTHENTICATION_TIMEOUT:
+      // console.log('Refreshing timeout');
+      clearTimeout(state.timeout);
+      return {
+        timeout: setTimeout(() => {
+          // console.log(`Timeout expired after ${
+          //   process.env.REACT_APP_AUTHENTICATION_TIMEOUT_SEC
+          // } seconds. Logging out happens now. This came from a refresh.`);
+        }, process.env.REACT_APP_AUTHENTICATION_TIMEOUT_SEC * 1000),
+      };
+    case types.START_AUTHENTICATION_TIMEOUT:
+      // console.log('Starting timeout');
+      return {
+        timeout: setTimeout(() => {
+          // Logout action goes here
+          // console.log(`Timeout expired after ${
+          //   process.env.REACT_APP_AUTHENTICATION_TIMEOUT_SEC
+          // } seconds. Logging out happens now.`);
+        }, process.env.REACT_APP_AUTHENTICATION_TIMEOUT_SEC * 1000),
+      };
+    case types.STOP_AUTHENTICATION_TIMEOUT:
+      // console.log('Stopping timeout');
+      clearTimeout(state.timeout);
+      return {
+        timeout: null,
+      };
+    default:
+      return state;
+  }
+};
+
 const defaultVersionsReducerState = {
   data: [],
 };
@@ -20,6 +60,7 @@ export const versionsReducer = (
 };
 
 const commonReducer = combineReducers({
+  authenticationTimeout: authenticationTimeoutReducer,
   versions: versionsReducer,
 });
 
